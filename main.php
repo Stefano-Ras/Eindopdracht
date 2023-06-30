7c4a4a1a46ab2577bb240dfc1ea749413f6438bd
Stefano Ras <stefano-ras@live.nl>
2023-06-30 10:49:13 +0200

    Adding files

---
+++ main.php
@@ -0,0 +1,15 @@
+<!--
+    Region:
+    - American  American cuisine is generally a type of fusion cuisine which
+                assimilates flavors from the melting pot of traditional American cooking techniques
+                mixed with foreign and sometimes molecular gastronomy components.
+    - Asian     Asian cuisine is local, authentic, and traditional.
+                It is rich in flavor and taste from the herbs and spices used in the preparations.
+                Asian cuisine features multicultural dishes from diverse cultures and origins
+                from southeast Asia, Europe, America, and around the globe.
+    - Dutch     Dutch food fucking sucks. Choose something else.
+    - European  The European cuisine includes a wide use of meat ranging from poultry to cattle,
+                sheep, pork and varied game. There are an infinite number of dairy products such as yogurt,
+                soft and hard cheeses. In coastal areas, all kinds of fish are present because
+                the European coasts are bathed by the main oceans and inland seas.
+-->
