30d07b70c4f9bdc1949c8e31be6df3933129e646
SR <stefano-ras@live.nl>
2024-04-26 11:07:18 +0200

    Added test file

--- main.php
+++ main.php
@@ -10,22 +10,22 @@
                     if ($conn->connect_error) {
                         # Display an error mesage if the connection fails
                         die("Connection failed: " . $conn->connect_error);
-                    }
-                /* 
+                    }?>
+                <!--?php*
                     $region = $conn->prepare("SELECT * FROM region");
                     $region = $conn->prepare("INSERT INTO region (Name, Description) VALUES (?, ?, ?)");
                     $region->bind_param("userregion", $Name, $Description);
 
                     file_put_contents("./recipe.json", json_encode($region));
-                */
                     //SELECT * FROM region WHERE Name = filtered name
                     $regionResult = $db("SELECT DISTINCT Region FROM Recipe ORDER BY Name ASC");
                     //$sql = "SELECT * FROM region;";
                     ?>
-                    <div class="search-box">
+                -->
+                    <!-- <div class="search-box">
                         <select id="region" name="region[]">
                             <option value="0" selected="selected">Select region</option>
-                            <?php
+                            <x?php
                                 if (! empty($regionResult)) {
                                      foreach ($regionResult as $key => $value) {
                                          echo '<option value="' . $regionResult[$key]['Region'] . '">' . $regionResult[$key]['Region'] . '</option>';
@@ -34,17 +34,28 @@
                             ?>
                         </select>
                         <button id="filter">Search</button>
-                    </div>
+                    </div> -->
                 <?php
                     $sql = "SELECT * FROM region;";
                     $result = $conn->query($sql);
                     $resultCheck = mysqli_num_rows($result);
                     if($resultCheck > 0) {
+                        echo "<select class='selection'>";
                         while($row = $result->fetch_assoc()) {
-                            echo $row['Name'] . "<br>" . $row['Description'] . "<br><br>";
+                            echo "<option>" . $row['Name'] . "</option>";
                         }
+                        echo "</select>";
                     }
 
+                    $sqlRecipe = "SELECT * FROM recipe;";
+                    $resultRecipe = mysqli_query($conn, $sqlRecipe);
+                    $resultCheckRecipe = mysqli_num_rows($resultRecipe);
+                    if($resultCheckRecipe > 0) {
+                        while($row = mysqli_fetch_assoc($resultRecipe)) {
+                            echo $row['Name'] . "<br><br>" . $row['Description'] . "<br><br>" . $row['Life_Story'] . "<br><br>" . $row['Instructions'] . "<br><br><hr/>";
+                        }
+                    }
+                    
                     $sqlIngredients = "SELECT * FROM ingredients;";
                     $resultIngredients = mysqli_query($conn, $sqlIngredients);
                     $resultCheckIngredients = mysqli_num_rows($resultIngredients);
@@ -53,16 +64,7 @@
                             echo $row['Amount'] . " " . $row['Unit'] . " " . $row['Name'] . "<br><br>";
                         }
                     }
-
-                    $sqlRecipe = "SELECT * FROM recipe;";
-                    $resultRecipe = mysqli_query($conn, $sqlRecipe);
-                    $resultCheckRecipe = mysqli_num_rows($resultRecipe);
-                    if($resultCheckRecipe > 0) {
-                        while($row = mysqli_fetch_assoc($resultRecipe)) {
-                            echo $row['Name'] . "<br><br>" . $row['Description'] . "<br><br>" . $row['Life_Story'] . "<br><br>" . $row['Instructions'] . "<br><br>";
-                        }
-                    }
-
+                    
                     //Join recipe & ingredients tables
                     //Show Recipe Name, Description, Story / Ingredients Amount, Unit, Name
 
@@ -74,3 +76,13 @@
 <?php
     $conn->close();
 ?>
+<style>
+    .selection {
+        margin: 0 auto 25px auto;
+        width: 250px;
+        height: 30px;
+        display: block;
+        font-size: 20px;
+        text-align: center;
+    }
+</style>
